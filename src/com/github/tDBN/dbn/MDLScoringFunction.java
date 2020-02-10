88f3882185d0da75493a9d4aa6858a7a1c8446f8
ttlion <tiago.miguel.leao@gmail.com>
2020-02-10 19:16:48 +0000

    mdl tb suporta as mudancas feitas para incluir attributos estaticos

--- src/com/github/tDBN/dbn/MDLScoringFunction.java
+++ src/com/github/tDBN/dbn/MDLScoringFunction.java
@@ -6,12 +6,18 @@ public class MDLScoringFunction extends LLScoringFunction {
 
 	@Override
 	public double evaluate(Observations observations, int transition, List<Integer> parentNodesPast,
-			Integer parentNodePresent, int childNode) {
-
-		LocalConfiguration c = new LocalConfiguration(observations.getAttributes(), observations.getMarkovLag(),
-				parentNodesPast, parentNodePresent, childNode);
-
-		double score = super.evaluate(observations, transition, parentNodesPast, parentNodePresent, childNode);
+			Integer parentNodePresent, int childNode, ObservationsStatic observStatic, List<Integer> staticParentSet) {
+		
+		LocalConfiguration c;
+		if(observStatic == null && staticParentSet == null ) {
+			c = new LocalConfiguration(observations.getAttributes(), observations.getMarkovLag(),
+					parentNodesPast, parentNodePresent, childNode);
+		} else {
+			c = new LocalConfigurationWithStatic(observations.getAttributes(), observations.getMarkovLag(),
+					parentNodesPast, parentNodePresent, childNode, observStatic.getAttributes(), staticParentSet);
+		}
+		
+		double score = super.evaluate(observations, transition, parentNodesPast, parentNodePresent, childNode, observStatic, staticParentSet);
 
 		// regularizer term
 		score -= 0.5 * Math.log(observations.numObservations(transition)) * c.getNumParameters();
