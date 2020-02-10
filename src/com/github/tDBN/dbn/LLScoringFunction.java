ecadc8fe31be463db166024481876fc7cc413446
ttlion <tiago.miguel.leao@gmail.com>
2020-02-10 18:31:08 +0000

    alteracoes as scoring functions para poderem suportar attributos estaticos

--- src/com/github/tDBN/dbn/LLScoringFunction.java
+++ src/com/github/tDBN/dbn/LLScoringFunction.java
@@ -5,25 +5,38 @@ import java.util.List;
 public class LLScoringFunction implements ScoringFunction {
 
 	@Override
-	public double evaluate(Observations observations, int transition, List<Integer> parentNodesPast, int childNode) {
-		return evaluate(observations, transition, parentNodesPast, null, childNode);
+	public double evaluate(Observations observations, int transition, List<Integer> parentNodesPast, int childNode, ObservationsStatic observStatic, List<Integer> staticParentSet) {
+		return evaluate(observations, transition, parentNodesPast, null, childNode, observStatic, staticParentSet);
 	}
 
 	@Override
 	public double evaluate(Observations observations, int transition, List<Integer> parentNodesPast,
-			Integer parentNodePresent, int childNode) {
-
-		LocalConfiguration c = new LocalConfiguration(observations.getAttributes(), observations.getMarkovLag(),
-				parentNodesPast, parentNodePresent, childNode);
+			Integer parentNodePresent, int childNode, ObservationsStatic observStatic, List<Integer> staticParentSet) {
+		
+		LocalConfiguration c;
+		if(observStatic == null && staticParentSet == null ) {
+			c = new LocalConfiguration(observations.getAttributes(), observations.getMarkovLag(),
+					parentNodesPast, parentNodePresent, childNode);
+		} else {
+			//System.out.println("Criando config com static!!");
+			c = new LocalConfigurationWithStatic(observations.getAttributes(), observations.getMarkovLag(),
+					parentNodesPast, parentNodePresent, childNode, observStatic.getAttributes(), staticParentSet);
+			
+			//System.out.println(c);
+		}
+		
+		
 
 		double score = 0;
 
 		do {
 			c.setConsiderChild(false);
-			int Nij = observations.count(c, transition);
+			int Nij = observations.count(c, transition, observStatic);
+			//System.out.println("Nij:" + Nij);
 			c.setConsiderChild(true);
 			do {
-				int Nijk = observations.count(c, transition);
+				int Nijk = observations.count(c, transition, observStatic);
+				//System.out.println("\t Nijk:" + Nijk);
 				if (Nijk != 0 && Nijk != Nij) {
 					score += Nijk * (Math.log(Nijk) - Math.log(Nij));
 				}
@@ -34,14 +47,14 @@ public class LLScoringFunction implements ScoringFunction {
 	}
 
 	@Override
-	public double evaluate(Observations observations, List<Integer> parentNodesPast, int childNode) {
-		return evaluate(observations, parentNodesPast, null, childNode);
+	public double evaluate(Observations observations, List<Integer> parentNodesPast, int childNode, ObservationsStatic observStatic, List<Integer> staticParentSet) {
+		return evaluate(observations, parentNodesPast, null, childNode, observStatic, staticParentSet);
 	}
 
 	@Override
 	public double evaluate(Observations observations, List<Integer> parentNodesPast, Integer parentNodePresent,
-			int childNode) {
-		return evaluate(observations, -1, parentNodesPast, parentNodePresent, childNode);
+			int childNode, ObservationsStatic observStatic, List<Integer> staticParentSet) {
+		return evaluate(observations, -1, parentNodesPast, parentNodePresent, childNode, observStatic, staticParentSet);
 	}
 
 }
--- src/com/github/tDBN/dbn/ScoringFunction.java
+++ src/com/github/tDBN/dbn/ScoringFunction.java
@@ -5,20 +5,20 @@ import java.util.List;
 public interface ScoringFunction {
 
 	public abstract double evaluate(Observations observations, int transition, List<Integer> parentNodesPast,
-			int childNode);
+			int childNode, ObservationsStatic observStatic, List<Integer> staticParentSet);
 
 	public abstract double evaluate(Observations observations, int transition, List<Integer> parentNodesPast,
-			Integer parentNodePresent, int childNode);
+			Integer parentNodePresent, int childNode, ObservationsStatic observStatic, List<Integer> staticParentSet);
 
 	/**
 	 * Calculate score when process is stationary.
 	 */
 	public abstract double evaluate(Observations observations, List<Integer> parentNodesPast,
-			Integer parentNodePresent, int childNode);
+			Integer parentNodePresent, int childNode, ObservationsStatic observStatic, List<Integer> staticParentSet);
 
 	/**
 	 * Calculate score when process is stationary.
 	 */
-	public abstract double evaluate(Observations observations, List<Integer> parentNodesPast, int childNode);
+	public abstract double evaluate(Observations observations, List<Integer> parentNodesPast, int childNode, ObservationsStatic observStatic, List<Integer> staticParentSet);
 
 }
